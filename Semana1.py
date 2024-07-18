742bad90eadb1b08f5f05c2776af29b7a5602ef4
Ruy Figueiredo <ruyfigueiredo.prof@gmail.com>
2024-07-18 19:05:06 -0300

    Arquivos da Semana 1 ML

---
+++ Semana1.py
@@ -0,0 +1,15 @@
+# NumPy para manipular matrizes e vetores
+import numpy as np
+
+## Pandas para manipular os dataframes
+import pandas as pd
+
+## Plotar figuras bonitas
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+import seaborn as sns
+print('Setup completo!')#
